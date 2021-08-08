8214504a3ae177a4221bd9c54ef8ae93014ce826
Chinye Daniel Ifechukwude <chinye@Chinyes-MacBook-Pro.local>
2021-08-08 19:11:37 +0000

    technify-shop CheckoutNav component started working, react-bootstrap dependencies giving errors

--- src/components/Checkout/CheckoutNav.js
+++ src/components/Checkout/CheckoutNav.js
@@ -1,9 +1,9 @@
+
 const CheckoutNav = () => {
   return (
     <div className="container">
-      <h4 className="display-6 text-center">Checkout Nav</h4>
     </div>
   );
 }
- 
+
 export default CheckoutNav;
